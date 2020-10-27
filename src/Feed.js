5d409a4fef4751cacbaedc9655b82cf606d931ef
alfaritsi21 <arqi.alfaritsi21@gmail.com>
2020-10-28 00:02:01 +0700

    feat: add component for posting and widgets

--- src/Feed.js
+++ src/Feed.js
@@ -10,9 +10,21 @@ function Feed() {
       <StoryReel />
       <MessageSender />
 
-      <Post />
-      <Post />
-      <Post />
+      <Post
+        profilePic="https://avatars3.githubusercontent.com/u/63988114?s=460&u=1dfc4cc474ab100d82e36f1144a5f005efbc0853&v=4"
+        message="Wow this works!"
+        timestamp="This is timestamp"
+        username="arqia"
+        image="https://www.telegraph.co.uk/content/dam/Travel/2018/August/adults%20iStock-672425798-xlarge.jpg"
+      />
+
+      <Post
+        profilePic="https://avatars3.githubusercontent.com/u/63988114?s=460&u=1dfc4cc474ab100d82e36f1144a5f005efbc0853&v=4"
+        message="Wow this works!"
+        timestamp="This is timestamp"
+        username="arqia"
+        image="https://www.telegraph.co.uk/content/dam/Travel/2018/August/adults%20iStock-672425798-xlarge.jpg"
+      />
     </div>
   );
 }
--- src/Post.js
+++ src/Post.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./Post.css";
 import { Avatar } from "@material-ui/core";
+import ThumbUpIcon from "@material-ui/icons/ThumbUp";
+import ChatBubbleOutlineIcon from "@material-ui/icons/ChatBubbleOutline";
+import AccountCircleIcon from "@material-ui/icons/AccountCircle";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import { ExpandMoreOutlined } from "@material-ui/icons";
 
 function Post({ profilePic, image, username, timestamp, message }) {
   return (
-    <div className="Post">
+    <div className="post">
       <div className="post__top">
         <Avatar src={profilePic} className="post__avatar" />
         <div className="post__topInfo">
@@ -16,6 +21,29 @@ function Post({ profilePic, image, username, timestamp, message }) {
 
       <div className="post__bottom">
         <p>{message}</p>
+      </div>
+
+      <div className="post__image">
+        <img src={image} alt="" />
+      </div>
+
+      <div className="post__options">
+        <div className="post__option">
+          <ThumbUpIcon />
+          <p>Like</p>
+        </div>
+        <div className="post__option">
+          <ChatBubbleOutlineIcon />
+          <p>Comment</p>
+        </div>
+        <div className="post__option">
+          <NearMeIcon />
+          <p>Share</p>
+        </div>
+        <div className="post__option">
+          <AccountCircleIcon />
+          <ExpandMoreOutlined />
+        </div>
       </div>
     </div>
   );
